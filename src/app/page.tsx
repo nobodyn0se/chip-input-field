f70d71b38a2245a901a13630ed6b7b9b80632aab
nobodyn0se <nobodyn0se@protonmail.ch>
2024-01-16 23:44:25 +0530

    Enhanced styling for selected chips

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 'use client';
-import { useEffect, useState } from "react";
+
+import { useEffect, useRef, useState } from "react";
+import React from "react";
+import IconCloseCircleOutline from "./components/CloseIcon";
 
 export default function Home() {
   const nameList: string[] = ['Nikita Khruchev', 'Harry S Truman', 'Catherine Margaret', 'Anna Vasquez', 'Marlo Hendricks'];
@@ -14,24 +17,38 @@ export default function Home() {
 
   const [debouncedInput, setDebouncedInput] = useState<string>('');
 
+  const newRef = useRef<any>(null);
+
+  // detects and handles click outside the input box
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  });
+
+  const handleOutsideClick = (e: any) => {
+    if (newRef.current && !newRef.current.contains(e.target)) {
+      handleOutOfFocus();
+    }
+  };
+
   const handleInputClick = () => {
     setShowSuggestion(true);
   };
 
   const handleOutOfFocus = () => {
-    const focusTimeout = setTimeout((() => setShowSuggestion(false)), 100);
+    setShowSuggestion(false);
     setSearchQuery('');
   };
 
   const handleOptionClick = (name: string) => {
-    console.log(`Option ${name} clicked`);
     setSelectedList([...selectedList, name]);
-    console.log(selectedList);
 
     setSuggestionList(suggestionList.filter((item) => item !== name));
     setPredictionList(predictionList.filter((item) => item !== name));
-    console.log(predictionList);
 
+    setSearchQuery('');
     setShowSuggestion(false);
   };
 
@@ -39,6 +56,7 @@ export default function Home() {
     setSelectedList(selectedList.filter((item) => item !== name));
     setSuggestionList([...suggestionList, name]);
     setPredictionList([...predictionList, name]);
+    setSearchQuery('');
   };
 
   const handleInputSearch = (event: any) => {
@@ -49,79 +67,55 @@ export default function Home() {
     const debounceTime = setTimeout(() => {
       setDebouncedInput(searchQuery);
     }, 700);
-    return () => clearTimeout(debounceTime);
-  }, [searchQuery]);
 
-
-  useEffect(() => {
-    let predictions = suggestionList.filter((item) => {
+    // filters the search results
+    const predictions = suggestionList.filter((item) => {
       return (searchQuery === '') ? suggestionList :
-      item.toLowerCase().includes(searchQuery.toLowerCase());
+        item.toLowerCase().includes(searchQuery.toLowerCase());
     });
 
     setPredictionList(predictions);
+
+    return () => clearTimeout(debounceTime);
   }, [searchQuery]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24">
-      <div>This will be the input box</div>
-      <div className="w-1/2 border-2 border-gray-300 rounded-md mt-4 p-3">
+    <main className="flex w-full min-h-screen flex-col items-center justify-start p-24">
+      <div>Click the input box to view names or type for search suggestions</div>
+      <div ref={newRef} className="w-1/2">
+        <div className="flex flex-wrap border-2 border-gray-300 rounded-md mt-4 p-3">
 
-        {
-          selectedList.length > 0 &&
-          selectedList.map((name, index) => (
-            <li className="list-none p-1" key={`option-${name}`}>
-              <div className="bg-zinc-400 max-w-fit rounded-3xl whitespace-nowrap p-2">{name}
-              <button onClick={() => handleDeleteClick(name)} className="ml-2 inline align-middle"><IconCloseCircleOutline/></button></div></li>
+          {/* Displays selected names in the form of chips */}
+          {
+            selectedList.length > 0 &&
+            selectedList.map((name, index) => (
+              <li className="list-none m-1 p-1" key={`option-${name}`}>
+                <div className="bg-zinc-400 max-w-fit rounded-3xl whitespace-nowrap p-2">{name}
+                  <button onClick={() => handleDeleteClick(name)} className="ml-2 inline align-middle"><IconCloseCircleOutline /></button></div></li>
             )
+            )
+          }
+          {
+            suggestionList.length > 0 &&
+            <input className="outline-none mt-1" type="text" placeholder="Type a name here"
+              value={searchQuery} onChange={handleInputSearch} onClick={handleInputClick} />
+          }
+        </div>
+        {
+          showSuggestion && (
+            <div className="border-2 border-gray-300 mt-1 rounded-md p-2">
+              {
+                predictionList.length > 0 ?
+                  predictionList.map((name, index) => (
+                    <div key={`option-${name}`} className="p-2 border-b-2 last:border-b-0 hover:bg-blue-300 cursor-pointer border-gray-300"
+                      onClick={() => handleOptionClick(name)}>{name}</div>
+                  )) : <div className="p-2 border-b-2 last:border-b-0 text-center text-slate-400 border-gray-300">No results found</div>
+              }
+            </div>
           )
         }
-        {
-          suggestionList.length > 0 &&
-          <input className="w-3/5 outline-none" type="text" placeholder="Type a name here"
-          value={searchQuery} onChange={handleInputSearch} onClick={handleInputClick} onBlur={handleOutOfFocus} />
-        }
-      </div>
-      {
-        showSuggestion && (
-          <div className="w-1/2 border-2 border-gray-300 mt-1 rounded-md p-2">
-            {
-              predictionList.map((name, index) => (
-                <div key={`option-${name}`} className="p-2 border-b-2 last:border-b-0 hover:bg-blue-300 cursor-pointer border-gray-300"
-                  onClick={() => handleOptionClick(name)}>{name}</div>
-              ))
-            }
-          </div>
-        )
-      }
+        </div>
+
     </main>
   )
-}
-
-const IconCloseCircleOutline = (props: any) => {
-  return (
-    <svg
-      viewBox="0 0 512 512"
-      fill="currentColor"
-      height="1.5em"
-      width="1.5em"
-      {...props}
-    >
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeMiterlimit={10}
-        strokeWidth={32}
-        d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z"
-      />
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={32}
-        d="M320 320L192 192M192 320l128-128"
-      />
-    </svg>
-  );
 }
